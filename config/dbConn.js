454a18d2d99a248855ff3a74758fc4427e21874d
Vegard Madsen <vegarsma@stud.ntnu.no>
2023-06-12 20:03:42 +0200

    fixing dbConn

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI)
-    } catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
